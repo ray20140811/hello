97ddec245c8161c050159eab34147d82dda91add
Ray Kuo <ray20140811@gmail.com>
2022-07-27 00:49:44 +0800

    4-3-1 URL Helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/math'
+  root 'pages#home'
+
+  #get 'pages/home'
+  #get 'pages/about'
+  #get 'pages/math'
+  get 'home', 'about', 'math', controller: 'pages'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
