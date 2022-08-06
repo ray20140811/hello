c523c2c08b685c5a1a89a7ef446bee1474eec0f2
Ray Kuo <ray20140811@gmail.com>
2022-08-06 10:45:48 +0800

    4-3-5 Input Helper: <input>改用對應的form helper

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   #get :home, :math, controller: :pages
   #get :about, controller: :pages, path: :me         # _navbar.html.erb     about_path # => '/me'
   get :home, :math, :about, :form, controller: :pages
+  #get :home, :math, :form, controller: :pages
+  #post :about, controller: :pages  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
